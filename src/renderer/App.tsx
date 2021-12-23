3f7630c9e6b8630faf7cb262e57c88d7d447bb5d
Not My Problem <notmyproblem@Nots-MacBook-Pro.local>
2021-12-22 21:18:07 -0500

    Button sends test message

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,26 +2,24 @@ import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
+const sendTestMessage = () => {
+  console.log("test")
+};
+
 const Hello = () => {
   return (
     <div>
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
+      <h1>Tabletop Simulator RPG Builder</h1>
+      <div className="Hello" onClick={sendTestMessage} >
+        <button type="button" id="file-test">
+          <span role="img" aria-label="books">
+            📚
+          </span>
+          Read our docs
+        </button>
         <a
           href="https://github.com/sponsors/electron-react-boilerplate"
           target="_blank"
