b27322d053ca0e667c1a364f6e0afe37c2ebcf90
DuncanUszkay1 <duncan.uszkay@gmail.com>
2021-12-25 16:02:26 -0500

    First draft visual for splash page

--- src/renderer/components/CampaignSelector.tsx
+++ src/renderer/components/CampaignSelector.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import icon from '../../../assets/icon.svg';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 export class CampaignSelector extends React.Component<{}, {}> {
   render() {
-    return <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>Tabletop Simulator RPG Builder</h1>
-      <div className="Hello" >
+    return <Box sx={{
+      width: '100%',
+      textAlign: 'center',
+      pt: '20%'
+    }}>
+      <Typography variant="h1" component="div" gutterBottom>
+        Ven
+      </Typography>
+      <Typography variant="h2" component="div" gutterBottom>
+        Tabletop Simulator RPG Editor
+      </Typography>
+      <Stack direction="row" spacing={2} justifyContent="center" alignItems="center" sx= {{
+        pt: "20px"
+      }}>
+        <Button variant="contained">
+          New Campaign
+        </Button>
+        <Button variant="contained">
+          Load Campaign
+        </Button>
         <IncrementButton />
-      </div>
-    </div>
+      </Stack>
+    </Box>
   }
 };
 
