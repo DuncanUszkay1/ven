fcc659eea425ff5e67f5bd9e79ecc2a2e4de1dd8
DuncanUszkay1 <duncan.uszkay@gmail.com>
2021-12-25 22:43:05 -0500

    Separate out components

--- src/renderer/components/Editor.tsx
+++ src/renderer/components/Editor.tsx
@@ -1,107 +1,53 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import TabPanel from '@mui/material/Tab';
 import Stack from '@mui/material/Stack';
 import { Typography } from '@mui/material';
 import { Campaign } from '../App';
-// import Typography from '@mui/material/Typography';
-// import Box from '@mui/material/Box';
-// import Stack from '@mui/material/Stack';
+import { Sidebar } from './Sidebar';
+import { TabPanel } from './TabPanel';
 
-export class Editor extends React.Component<{ campaign: Campaign }, {}> {
+export class Editor extends React.Component<{ campaign: Campaign }, { section: number }> {
+  state = { section: 0 } 
+
+  constructor(props: { campaign: Campaign }) {
+    super(props);
+
+    this.updatePanel = this.updatePanel.bind(this);
+  }
+
+  updatePanel(newValue: number) {
+    this.setState({ section: newValue })
+  }
+
   render() {
+    console.log('rerending')
+    console.log(this.state)
     return <Stack sx={{ height: "100%" }}>
       <Box sx={{ width: "100%", borderBottom: "1px solid grey", padding: "10px" }}>
         <Typography variant="button">{this.props.campaign.name}</Typography>
       </Box>
       <Stack direction="row" sx={{ height: "100%" }}>
-        <VerticalTabs />
+        <Box
+          sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}
+        >
+          <Sidebar updatePanel={this.updatePanel}/>
+          <TabPanel value={this.state.section} index={0}>
+            Overview content here
+          </TabPanel>
+          <TabPanel value={this.state.section} index={1}>
+            Characters content here
+          </TabPanel>
+          <TabPanel value={this.state.section} index={2}>
+            Items content here
+          </TabPanel>
+          <TabPanel value={this.state.section} index={3}>
+            Maps content here
+          </TabPanel>
+          <TabPanel value={this.state.section} index={4}>
+            Files content here
+          </TabPanel>
+        </Box>
       </Stack>
     </Stack> 
   }
 };
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
-}
-
-export default function VerticalTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex'}}
-    >
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
-      >
-        <Tab label="Overview" {...a11yProps(0)} />
-        <Tab label="Characters" {...a11yProps(1)} />
-        <Tab label="Items" {...a11yProps(2)} />
-        <Tab label="Maps" {...a11yProps(3)} />
-        <Tab label="Files" {...a11yProps(4)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-    </Box>
-  );
-}
---
+++ src/renderer/components/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export function Sidebar(props: { updatePanel: (newValue: number) => void }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    props.updatePanel(newValue);
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs
+      orientation="vertical"
+      variant="scrollable"
+      value={value}
+      onChange={handleChange}
+      aria-label="Vertical tabs example"
+      sx={{ borderRight: 1, borderColor: 'divider' }}
+    >
+      <Tab label="Overview" {...a11yProps(0)} />
+      <Tab label="Characters" {...a11yProps(1)} />
+      <Tab label="Items" {...a11yProps(2)} />
+      <Tab label="Maps" {...a11yProps(3)} />
+      <Tab label="Files" {...a11yProps(4)} />
+    </Tabs>
+  );
+}
