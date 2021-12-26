997bc3cc6a871b164392b609a99671ae7efb5694
DuncanUszkay1 <duncan.uszkay@gmail.com>
2021-12-25 23:27:01 -0500

    Skeleton of character editor missing change

---
+++ src/renderer/components/TiledExplorer.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import SelectedList from './SelectableList';
+import { Typography } from '@mui/material';
+
+export default class TiledExplorer extends React.Component {
+  render() {
+    return <Stack direction="row" sx={{flexWrap: "wrap"}}>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+      <Typography variant="h3">BONANZA</Typography>
+    </Stack>
+  }
+}
